226d8ed2426a8530f84eb7c13af8fe455187801e
Bryan Bachelet <b.bachelet@student.isartdigital.com>
2023-10-12 14:16:51 +0200

    Finish V1 Portfolio

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,6 +1,27 @@
 
-setTimeout(myTimer, 1000)
 
+const aboutMe = document.querySelector(".AboutMe");
+const buttonProjec = document.querySelector(".Project");
+const ContactProject = document.querySelector(".Contact");
+
+const pres = document.querySelector(".aboutme");
+const footer = document.querySelector(".Footer")
+const project = document.querySelector(".Galerie")
+
+aboutMe.addEventListener("click", function () {
+    pres.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+});
+
+buttonProjec.addEventListener("click", function () {
+  project.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+});
+
+ContactProject.addEventListener("click", function () {
+  footer  .scrollIntoView({ behavior: "smooth", block: "end", inline: "end" });
+});
+
+
+setTimeout(myTimer, 1000)
 function myTimer() {
     var h2 = document.getElementById("content");
     h2.classList.add("test");
@@ -8,3 +29,5 @@ function myTimer() {
     var hint = document.getElementById("hint")
     hint.classList.add("hintVisible")
   }
+
+  
