fb1c8748aa7d512627ac90f12433ecf1970f1bc2
Bryan Bachelet <bryanbacheletpro@gmail.com>
2025-01-15 07:10:28 +0100

    Finish Front Page

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -4,12 +4,25 @@ const aboutMe = document.querySelector(".AboutMe");
 const buttonProjec = document.querySelector(".Project");
 const ContactProject = document.querySelector(".Contact");
 
+const galerieTest =  document.querySelector(".GalerieContainer").querySelectorAll(".Project");
+const galerieTest2 =  document.querySelector(".GalerieContainer");
+
+let currentGalerie;
+
 const pres = document.querySelector(".aboutme");
 const footer = document.querySelector(".Footer")
 const project = document.querySelector(".Galerie")
 
 aboutMe.addEventListener("click", function () {
-    pres.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  var element = document.querySelector('.aboutme');
+  var headerOffset = 45;
+    var elementPosition = element.getBoundingClientRect().top;
+  var offsetPosition = elementPosition + window.scrollY - headerOffset;
+  
+  window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth"
+  });   
 });
 
 buttonProjec.addEventListener("click", function () {
@@ -17,14 +30,55 @@ buttonProjec.addEventListener("click", function () {
 });
 
 ContactProject.addEventListener("click", function () {
-  footer  .scrollIntoView({ behavior: "smooth", block: "end", inline: "end" });
+  var element = document.querySelector('.Footer');
+  var headerOffset = 45;
+    var elementPosition = element.getBoundingClientRect().top;
+  var offsetPosition = elementPosition + window.scrollY - headerOffset;
+  
+  window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth"
+  });   
 });
+
+
+
+for( let project of galerieTest)
+{
+    project.addEventListener("mouseover", function()
+  {
+        galerieTest2.classList.add("Test")
+      var elements = [];
+      elements = document.querySelector(".GalerieContainer").getElementsByClassName("Project")
+
+      for(let element of elements)
+      { 
+        if(this != element) element.style.width = "150px";
+        else element.style.width = "1000px"
+      }
+  });
+
+  project.addEventListener("mouseleave", function()
+  {
+    galerieTest2.classList.remove("Test")
+
+
+    var elements = [];
+    elements = document.querySelector(".GalerieContainer").getElementsByClassName("Project")
+
+    for(let element of elements)
+    { 
+        element.style.width = "250px";
+    }
+  });
+}
+
 
 
 setTimeout(myTimer, 1000)
 function myTimer() {
     var h2 = document.getElementById("content");
-    h2.classList.add("test");
+   h2.classList.add("test");
 
     var hint = document.getElementById("hint")
     hint.classList.add("hintVisible")
